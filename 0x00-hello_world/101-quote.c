73e8e73cad25b7635885d6168d54d0d0c2ea22b5
esmond-adjei <xmongram@gmail.com>
2022-12-08 18:29:31 -0800

    modified some files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-
+/**
+ * main - outputs to string
+ *
+ * Description: Prints out "and that piece of art is useful..." without printf
+ * Return: 1
+ */
 int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
