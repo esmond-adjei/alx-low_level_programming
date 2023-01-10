95a67e2f1eb70cd85a3ccbc6f201b184c9645ee9
esmond-adjei <xmongram@gmail.com>
2023-01-10 13:51:39 -0800

    modified some files

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,30 +2,6 @@
 #include <stdlib.h>
 
 /**
- * wrdcnt - counts the number of words in a string
- * @s: string to count
- *
- * Return: int of number of words
- */
-int wrdcnt(char *s)
-{
-	int i, n = 0;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
-	}
-	n++;
-	return (n);
-}
-
-/**
  * strtow - splits a string into words
  * @str: string to split
  *
